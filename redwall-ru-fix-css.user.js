2d7de576798330ec0fee89d310c3ae697bf941f6
DearFox <59219907+DearFox@users.noreply.github.com>
2021-02-10 01:56:22 +0300

    0.0.1

--- redwall-ru-fix-css.user.js
+++ redwall-ru-fix-css.user.js
@@ -1,1 +1,26 @@
+// ==UserScript==
+// @name         redwall.ru Фикс стиля
+// @namespace    https://www.furaffinity.net/user/dearfox/
+// @version      0.0.1
+// @description  Этот юзерскрипт испровляет косяки в стилях сайта redwall.ru. Просто потому-что я перфекционист и люблю когда все идеально.
+// @author       DearFox
+// @match        https://www.redwall.ru/*
+// @match        http://www.redwall.ru/*
+// @downloadUrl  https://github.com/DearFox/UserScript-redwall.ru/raw/main/redwall-ru-fix-css.user.js
+// @updateUrl    https://github.com/DearFox/UserScript-redwall.ru/raw/main/redwall-ru-fix-css.user.js
+// @grant        GM_addStyle
+// ==/UserScript==
 
+(function() {
+    'use strict';
+GM_addStyle(`
+.ipsUserPhoto {
+    background: #ffffff00;}
+.ipsMemberCard {
+    background: #3d3930;
+    border: 1px solid #353029;}
+.ipsType_unbold {
+    color: #A79481 !important;}
+`);
+    // Your code here...
+})();
